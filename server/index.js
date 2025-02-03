da11937ce65c68018f2c3d52bbea58ec1976939a
Chirag Nanda <chiragnanda@Chirags-MacBook-Air.local>
2025-02-04 01:36:37 +0530

    frontend tester commit

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 const app= express();
 
 dotenv.config();
-app.use(cors());
+app.use(cors({origin : 'https://ogchefgptclient.vercel.app'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
