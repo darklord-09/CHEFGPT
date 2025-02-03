083aed838a3684f867fc4fc80f7aa4ea9c21ce1f
Chirag Nanda <chiragnanda@Chirags-MacBook-Air.local>
2025-02-03 22:45:57 +0530

    vercel commit

--- server/index.js
+++ server/index.js
@@ -65,9 +65,10 @@ app.post('/findUser',(req,result)=>{
   );
 })
 
+const PORT = process.env.PORT||3000
 
-app.listen(3000, ()=>{
-  console.log('Server running on port 3000')
+app.listen(PORT, ()=>{
+  console.log(`Server running on port ${PORT}`)
   
 });
 
